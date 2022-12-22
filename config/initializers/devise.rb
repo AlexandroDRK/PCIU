400a65da2156be0fab97ef88cf9bfc8db8028fa4
AlexandroDRK <alexsandro060302@gmail.com>
2022-12-22 09:21:48 -0300

    Corrigindo erros de validacão no Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,17 @@
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      :redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w[html turbo_stream].include? request_format.to_s
+  end
+end
+
 # frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
@@ -10,7 +24,12 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
+  config.parent_controller = 'TurboDeviseController'
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -311,3 +330,4 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
+  
