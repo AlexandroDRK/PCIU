9eb6af501bd9998fccbf5f25d9e5970f82f28e94
Blenda Costa <blen@blenda-w11>
2023-01-15 21:03:26 -0300

    Adição de seeds para o uso nos testes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Usuario.create([
+    {matricula: 1, email: 'prof@pciu.br', nome: 'Professor', tipo: 'Professor', password: 'senhasenha'},
+    {matricula: 2, email: 'aluno@pciu.br', nome: 'Aluno', tipo: 'Aluno', password: 'senhasenha'}
+    ])
+
+Curso.create(id: 1, nome: 'Ciência da Computação')
+t = Turma.create([{id: 11, nome: 'Bloco 06', curso_id: 1, periodo: '2022.1'}, {id: 12, nome: 'Bloco 08', curso_id: 1, periodo: '2022.1'}])
+
+u = Usuario.first
+u.turmas << t
+
+u = Usuario.last
+t = Turma.first
+u.turmas << t
